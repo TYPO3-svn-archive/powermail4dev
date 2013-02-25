15dfa3197247ade038080c43a6c4efd0e048b88e
dwildt <dwildt@735d13b6-9817-0410-8766-e36946ffe9aa>
2013-02-25 20:02:39 +0000

    0.0.1 +++ #45832
    git-svn-id: https://svn.typo3.org/TYPO3v4/Extensions/powermail4dev/trunk@71525 735d13b6-9817-0410-8766-e36946ffe9aa

--- lib/userfunc/class.tx_powermail4dev_userfunc.php
+++ lib/userfunc/class.tx_powermail4dev_userfunc.php
@@ -235,7 +235,7 @@ class tx_powermail4dev_userfunc
  */
   public function ffPowermailUid( $arr_pluginConf )
   {
-    $prompt = var_export( $arr_pluginConf, true );
+    echo '<pre>' .  var_dump( $arr_pluginConf ) . '</pre>';
     $prompt = $prompt . '<hr />';
     $prompt = $prompt . 2680;
     return $prompt;
