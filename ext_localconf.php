3a5bfd54212a38c01b0ee26a8d93d2066ca08b3e
dwildt <dwildt@735d13b6-9817-0410-8766-e36946ffe9aa>
2013-03-06 15:40:38 +0000

    0.0.2 +++ #i0001
    git-svn-id: https://svn.typo3.org/TYPO3v4/Extensions/powermail4dev/trunk@72325 735d13b6-9817-0410-8766-e36946ffe9aa

--- ext_localconf.php
+++ ext_localconf.php
@@ -17,6 +17,8 @@ if (!defined ('TYPO3_MODE'))  die ('Access denied.');
   //
   // addPItoST43
   
-t3lib_extMgm::addPItoST43($_EXTKEY,'pi1/class.tx_powermail4dev_pi1.php','_pi1','list_type',1);
+  // #i0001, 130306, dwildt;
+$cached = true;
+t3lib_extMgm::addPItoST43( $_EXTKEY, 'pi1/class.tx_powermail4dev_pi1.php','_pi1', 'list_type', $cached );
   // addPItoST43
 ?>
